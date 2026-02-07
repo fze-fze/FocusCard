d21960212ee710512f6a7649ce3b447a2c4081c6
fze <fanzeen451@gmail.com>
2026-02-07 18:32:42 +0800

    Initial Commit

---
+++ FocusCard/FocusCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusCardApp.swift
+//  FocusCard
+//
+//  Created by fze on 2026/2/7.
+//
+
+import SwiftUI
+
+@main
+struct FocusCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
